15aba43383a551d8a121b7f47bef5bc23e1874b3
Morthoke <56011195+Morthoke@users.noreply.github.com>
2023-03-24 23:50:37 +1100

    Delete Version 3.0 directory

--- Version 3.0/location.lua
+++
@@ -1,8 +0,0 @@
--- Add following line to "startup.lua"
--- shell.run("location")
-x,y,z = gps.locate()
-f = io.open("location.txt", "w+")
-f:write(tostring(x) .. "\n")
-f:write(tostring(y) .. "\n")
-f:write(tostring(z) .. "\n")
-f:close()
